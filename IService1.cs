6f3b96aae9e1a2e929e4f5dfdfd93886ae305b08
durgeshintelli <intellinet.systems@gmail.com>
2017-06-07 17:13:02 +0530

    Add files via upload

---
+++ IService1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using ApiAiSDK.Model;
+using Newtonsoft.Json.Linq;
+
+
+namespace AiChatService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        //[OperationContract]
+        //[WebGet(ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "ValidateUser/{sUserName}/{sPassword}")]
+        //List<ValidateUser> ValidateUser(string sUserName, string sPassword);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "ValidateUser")]
+        List<ValidateUser> ValidateUser(AIResponse ai);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.WrappedRequest, UriTemplate = "GetTestResponse")]
+        AiResult GetTestResponse(AIResponse ai);
+    }
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class ValidateUser
+    {
+        Int32 id = 0;
+
+
+        [DataMember]
+        public Int32 ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+
+    }
+      # region AI
+    public class AiResult
+    {
+        string Speech = string.Empty;
+        string DisplayText = string.Empty;
+        string Source = string.Empty;
+
+        public string speech
+        {
+            get { return Speech; }
+            set { Speech = value; }
+        }
+        public string displayText
+        {
+            get { return DisplayText; }
+            set { DisplayText = value; }
+        }
+        public string source
+        {
+            get { return Source; }
+            set { Source = value; }
+        }
+        public object data
+        {
+            get;
+            set;
+        }
+        public object[] contextOut
+        {
+            get;
+            set;
+        }
+    }
+
+    public class AIResponse
+    {
+        public string Id { get; set; }
+        public DateTime timestamp { get; set; }
+        public string lang { get; set; }
+        public Result result { get; set; }
+        public Status status { get; set; }
+        public string sessionId { get; set; }
+        public Fulfillment fulfillment { get; set; }
+        public bool IsError
+        {
+            get
+            {
+                if (status != null && status.code.HasValue && status.code >= 400)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+        }
+    }
+    public class Status
+    {
+        public int? code { get; set; }
+        public string errorType { get; set; }
+        public string errorDetails { get; set; }
+        public string errorID { get; set; }
+        public Status()
+        {
+        }
+    }
+
+
+    public class Result
+    {
+        String action;
+        public Boolean ActionIncomplete { get; set; }
+        public String Action
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(action))
+                {
+                    return string.Empty;
+                }
+                return action;
+            }
+            set
+            {
+                action = value;
+            }
+        }
+
+        public Dictionary<string, object> parameters { get; set; }
+        public AIOutputContext[] contexts { get; set; }
+        public Metadata Metadata { get; set; }
+        public String resolvedQuery { get; set; }
+        public Fulfillment fulfillment { get; set; }
+        public string source { get; set; }
+        public float score { get; set; }
+        public bool hasParameters
+        {
+            get
+            {
+                return parameters != null && parameters.Count > 0;
+            }
+        }
+
+        public string GetStringParameter(string name, string defaultValue = "")
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException("name");
+            }
+
+            if (parameters.ContainsKey(name))
+            {
+                return parameters[name].ToString();
+            }
+
+            return defaultValue;
+        }
+
+        public int GetIntParameter(string name, int defaultValue = 0)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException("name");
+            }
+
+            if (parameters.ContainsKey(name))
+            {
+                var parameterValue = parameters[name].ToString();
+                int result;
+                if (int.TryParse(parameterValue, NumberStyles.Integer, CultureInfo.InvariantCulture, out result))
+                {
+                    return result;
+                }
+
+                float floatResult;
+                if (float.TryParse(parameterValue, NumberStyles.Float, CultureInfo.InvariantCulture, out floatResult))
+                {
+                    result = Convert.ToInt32(floatResult);
+                    return result;
+                }
+            }
+
+            return defaultValue;
+        }
+
+        public float GetFloatParameter(string name, float defaultValue = 0)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException("name");
+            }
+
+            if (parameters.ContainsKey(name))
+            {
+                var parameterValue = parameters[name].ToString();
+                float result;
+                if (float.TryParse(parameterValue, NumberStyles.Float, CultureInfo.InvariantCulture, out result))
+                {
+                    return result;
+                }
+            }
+
+            return defaultValue;
+        }
+
+        public JObject GetJsonParameter(string name, JObject defaultValue = null)
+        {
+            if (string.IsNullOrEmpty("name"))
+            {
+                throw new ArgumentNullException(name);
+            }
+
+            if (parameters.ContainsKey(name))
+            {
+                var parameter = parameters[name] as JObject;
+                if (parameter != null)
+                {
+                    return parameter;
+                }
+            }
+
+            return defaultValue;
+        }
+
+        public AIOutputContext GetContext(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentException("Name must be not empty", name);
+            }
+
+            return contexts.FirstOrDefault(c => string.Equals(c.Name, name, StringComparison.CurrentCultureIgnoreCase));
+        }
+
+        public Result()
+        {
+        }
+    }
+
+    #endregion
+
+
+   
+}
---
+++ Service1.svc.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using ApiAiSDK.Model;
+using ApiAiSDK;
+using Newtonsoft.Json;
+
+namespace AiChatService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        private const string ACCESS_TOKEN = "a7487a128bf54f599a084c067cdfd5ce";
+        Service1()
+        {
+        }
+
+        public List<ValidateUser> ValidateUser(AIResponse ai)
+        {
+          
+
+            var testObject = new
+            {
+                id = "2d2d947b-6ccd-4615-8f16-59b8bfc0fa6b",
+                timestamp = "2017-07-13T11:03:43.023Z",
+                result = new
+                {
+                    source = "agent",
+                    resolvedQuery = "test params 1.23",
+                    speech = "manoj",
+                    action = "test_params",
+                    parameters = new
+                    {
+                        number = "1.23",
+                        integer = "17",
+                        str = "string value",
+                        complex_param = new { nested_key = "nested_value" }
+                    },
+                    metadata = new
+                    {
+                        intentId = "46a278fb-0ffc-4748-aa9a-5563d89199ee",
+                        intentName = "test params"
+                    }
+                },
+                fulfillment = new { speech = "chandan" },
+                status = new { code = 200, errorType = "success" }
+            };
+
+            var jsonText = JsonConvert.SerializeObject(testObject);
+            var config = new AIConfiguration(ACCESS_TOKEN, SupportedLanguage.English);
+
+            var apiAi = new ApiAi(config);
+
+            var response = apiAi.TextRequest("hh");
+            List<ValidateUser> objvalidate = new List<ValidateUser>();
+            ValidateUser vu = new ValidateUser();
+            vu.ID = 3;
+            objvalidate.Add(vu);
+            return objvalidate.ToList();
+        }
+
+        # region AI function
+
+        public AiResult GetTestResponse(AIResponse ai)
+        {
+            //var testObject = new
+            //{
+            //    id = "2d2d947b-6ccd-4615-8f16-59b8bfc0fa6b",
+            //    timestamp = "2017-07-13T11:03:43.023Z",
+            //    result = new
+            //    {
+            //        source = "agent",
+            //        resolvedQuery = "test params 1.23",
+            //        speech = "manoj",
+            //        action = "test_params",
+            //        parameters = new
+            //        {
+            //            number = "1.23",
+            //            integer = "17",
+            //            str = "string value",
+            //            complex_param = new { nested_key = "nested_value" }
+            //        },
+            //        metadata = new
+            //        {
+            //            intentId = "46a278fb-0ffc-4748-aa9a-5563d89199ee",
+            //            intentName = "test params"
+            //        }
+            //    },
+            //    fulfillment = new { speech = "chandan" },
+            //    status = new { code = 200, errorType = "success" }
+            //};
+
+            //var jsonText = JsonConvert.SerializeObject(testObject);
+            //return JsonConvert.DeserializeObject<AIResponse>(jsonText);
+            List<AiResult> objre=new List<AiResult> ();
+            AiResult air = new AiResult();
+            
+
+            return air;
+        }
+
+        # endregion
+    }
+}
